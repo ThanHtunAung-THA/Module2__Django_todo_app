a0d4508959ab3448819c23f3a9f75d14ed801132
BuggyCody <31753974+ThanHtunAung-THA@users.noreply.github.com>
2024-06-17 01:24:55 +0630

    CRUD for todo_list

--- myapp/url.py
+++ myapp/url.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
 
-    path('list/', views.todo_list, name='todo_list'),
+    path('todolist/', views.todo_list, name='todo_list'),
+    path('todocreatelist/', views.todo_create, name='todo_create'),
+    path('tododelete/<int:id>/', views.todo_delete, name='todo_delete'),
+    path('complete/<int:id>/', views.todo_complete, name='todo_complete'),
+    path('todoupdate/<int:id>/', views.todo_update, name='todo_update'),
 
 ]
+
+# m -> v -> t-> u -> u
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Todo
+
 # Create your views here.
 def todo_list(request):
     todos = Todo.objects.all()
     return render(request, 'todo_list.html', {'todos': todos})
 
+
+def todo_create(request):
+    if request.method == 'POST':
+        new_todo = Todo(
+            title = request.POST.get('title', ''),
+            description = request.POST.get('description', '')
+        )
+        if new_todo.title:
+            new_todo.save()
+            return redirect('todo_list')
+        else:
+            return render(request, 'todo_form.html', {'error': 'title cannot be empty.'})
+        
+    return render(request, 'todo_list.html')
+
+
+def todo_delete(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('todo_list')
+    return render(request, 'todo_confirm_delete.html', {'todo' : todo})
+
+
+def todo_complete(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    todo.completed = not todo.completed
+    todo.save()
+    return redirect('todo_list')
+
+
+def todo_update(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    if request.method == 'POST':
+        todo.title = request.POST.get('title', todo.title)
+        todo.description = request.POST.get('description', todo.description)
+        if todo.title:
+            todo.save()
+            return redirect('todo_list')
+        else:
+            return render(request, 'todo_form.html', {'todo': todo, 'error': 'Title cannot be empty.' })
+
+    return render(request, 'todo_edit.html', {'todo': todo})
+        
+# crud -> post, get ,ged, delete
